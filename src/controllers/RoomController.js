728e46fd66087568430b24b0f1959961ff4877f3
rafaelsouzabe <rafaelsouzabernado@gmail.com>
2021-07-02 13:45:17 -0300

    Foi adcionado, a mensagem de sem perguntas no room, corrigido a verificação de senha para marcar como lido e excluir perguntas

--- src/controllers/RoomController.js
+++ src/controllers/RoomController.js
@@ -1,3 +1,4 @@
+const { open } = require("sqlite")
 const Database = require("../db/config")
 
 module.exports = {
@@ -7,33 +8,62 @@ module.exports = {
             
             const db = await Database()
             const pass = req.body.password
-            
             let roomID = Math.floor(Math.random() * 10).toString()
+            let isRoom = true
+
+            while(isRoom){
+
+
+                // GERA O NUMERO DA SALA
+                
+                for(var i = 0; i < 5; i++){
+                    roomID += Math.floor(Math.random() * 10).toString()
+                }
+        
+                // VERIFICA SE O NUMERO JA EXISTE
+                const roomsExistIDS = await db.all(`SELECT id FROM rooms`)
+                isRoom = roomsExistIDS.some(roomsExistIDS => roomsExistIDS == roomID)
     
-    
-            for(var i = 0; i < 5; i++){
-                roomID += Math.floor(Math.random() * 10).toString()
+                if(!isRoom){
+                    // INSERI A SALA NO BANCO
+                    await db.run(`INSERT INTO rooms (
+                        id, 
+                        pass
+                    ) VALUES (
+                        ${parseInt(roomID)},
+                        ${pass}
+            
+                    )`);
+                }
             }
-    
-    
-            await db.run(`INSERT INTO rooms (
-                id, 
-                pass
-            ) VALUES (
-                ${parseInt(roomID)},
-                ${pass}
-    
-            )`);
-    
+
             await db.close()
             
             res.redirect(`/room/${roomID}` )
 
-
-
         } catch (error) {
             console.log(error)
         }
+    
+    },
+    async open(req, res){
+        const db = await Database()
+        const roomID = req.params.room
+        const questions = await db.all(`SELECT * FROM questions WHERE room = ${roomID} and read = 0`)
+        const questionsRead = await db.all(`SELECT * FROM questions WHERE room = ${roomID} and read = 1`)
 
+        let isNoQuestions 
+        if (questions.length == 0) {
+            if (questionsRead.length == 0) {
+                isNoQuestions = true
+            }
+        }
+        res.render("room", {roomID: roomID, questions: questions, questionsRead: questionsRead, isNoQuestions: isNoQuestions})
+    },
+
+    enter(req, res){
+        const roomID = req.body.roomID
+
+        res.redirect(`/room/${roomID}`)
     }
 }
--- src/route.js
+++ src/route.js
@@ -7,9 +7,11 @@ const route = express.Router()
 route.get('/', (req, res) => res.render("index",{page: 'enter-room'}))
 route.get('/create-pass', (req,res) => res.render("index",{page: 'create-pass'} ))
 
-route.get('/room/:room', (req,res) => res.render("room"))
-// formato que o formulario de dentro da modal tem que passar a informaçao
-route.post('/question/:room/:question/:action', QuestionController.index)
 route.post('/create-room', RoomController.create)
+route.get('/room/:room', RoomController.open)
+route.post('/enterroom', RoomController.enter)
 
+//  se estiver enviando alguma rota pra chamar eh post, se estiver so chamando a rota pra ler eh get
+route.post('/question/create/:room', QuestionController.create)
+route.post('/question/:room/:question/:action', QuestionController.index)
 module.exports = route
